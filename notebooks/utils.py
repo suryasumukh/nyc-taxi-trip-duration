194a61dee69a19d575e5874f4d10b93d17680b72
Surya Sumukh <ss3345@cornell.edu>
2017-07-27 23:39:12 -0700

    code: Update utils.py with more features. Tuned simple-mlp to work with new features.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,25 +1,45 @@
 import pandas as pd
 import numpy as np
 from scipy.spatial.distance import cityblock
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from datetime import datetime
+from haversine import haversine
 
+
+cal = USFederalHolidayCalendar()
+holidays = cal.holidays(start=datetime(2015, 12, 31),
+                        end=datetime(2017, 1, 1))
+
+
+def distance(positions):
+    lats = [(positions[0], 0.0), (positions[2], 0.0)]
+    longs = [(0.0, positions[1]), (0.0, positions[3])]
+    dist = (haversine(lats[0], lats[1]) + haversine(longs[0], longs[1]))/1000
+    return dist
 
 def preprocess(filepath):
     assert filepath is not None
     data = pd.read_csv(filepath, parse_dates=['pickup_datetime'])
     
-    data['pickup_hr'] = map(lambda dt: dt.hour, data['pickup_datetime'])
-    data['pickup_min'] = map(lambda dt: dt.minute, data['pickup_datetime'])
-    data['pickup_sec'] = map(lambda dt: dt.second, data['pickup_datetime'])
-    data['pickup_day'] = map(lambda dt: dt.dayofweek, data['pickup_datetime'])
-    data['pickup_date'] = map(lambda dt: dt.day, data['pickup_datetime'])
-    data['pickup_mon'] = map(lambda dt: dt.month, data['pickup_datetime'])
-    data['pickup_yr'] = map(lambda dt: dt.year, data['pickup_datetime'])
+    data['pickup_hr'] = data['pickup_datetime'].dt.hour
+    data['pickup_min'] = data['pickup_datetime'].dt.minute
+    data['pickup_sec'] = data['pickup_datetime'].dt.second
+    data['pickup_day'] = data['pickup_datetime'].dt.dayofweek
+    data['pickup_date'] = data['pickup_datetime'].dt.day
+    data['pickup_mon'] = data['pickup_datetime'].dt.month
+    
+    data['quarter'] = data['pickup_datetime'].dt.quarter
+    data['weekday'] = (data['pickup_day'] < 5).astype(int)
+    
+    data['holiday'] = (data['pickup_datetime'].dt.date.astype('datetime64').isin(holidays)).astype(int)
     
     data = data.assign(st_dist=lambda df: np.sqrt((df.pickup_longitude-df.dropoff_longitude)**2 + 
                                        (df.pickup_latitude-df.dropoff_latitude)**2))
     
-    y = data[[u'pickup_longitude', u'pickup_latitude', u'dropoff_longitude', u'dropoff_latitude']]
-    data['cityblock'] = map(lambda x: cityblock(x[0:2], x[2:]), y.as_matrix())
+    y = data[[u'pickup_latitude', u'pickup_longitude', 
+          u'dropoff_latitude', u'dropoff_longitude']]
+    data['h_dist'] = map(lambda pos: distance(pos), y.as_matrix())
+    
     data['s_pickup_latitude'] = (data['pickup_latitude'] - data['pickup_latitude'].min())/(data['pickup_latitude'].max() - data['pickup_latitude'].min())
     data['s_pickup_longitude'] = (data['pickup_longitude'] - data['pickup_longitude'].min())/(data['pickup_longitude'].max() - data['pickup_longitude'].min())
 
@@ -27,5 +47,8 @@ def preprocess(filepath):
     data['s_dropoff_longitude'] = (data['dropoff_longitude'] - data['dropoff_longitude'].min())/(data['dropoff_longitude'].max() - data['dropoff_longitude'].min())
     
     data['flag'] = (data['store_and_fwd_flag'] == 'Y').astype(int)
+    
+    data['month_end'] = (data['pickup_datetime'].dt.is_month_end).astype(int)
+    data['month_start'] = (data['pickup_datetime'].dt.is_month_start).astype(int)
     
     return data
